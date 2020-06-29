4e969d6fabdb4807522a45cd7a9b6763b85bb972
Steve <skirkendall@gmail.com>
2020-06-29 17:30:46 +0100

    Carousel styling part 1

--- js/folio1.js
+++ js/folio1.js
@@ -13,23 +13,7 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 
 // Get the modals
     var modal0101 = document.getElementById("modal01-01");
-    var modal0102 = document.getElementById("modal01-02");
-    var modal0103 = document.getElementById("modal01-03");
-    var modal0104 = document.getElementById("modal01-04");
-    var modal0105 = document.getElementById("modal01-05");
-    var modal0106 = document.getElementById("modal01-06");
-    var modal0107 = document.getElementById("modal01-07");
-    var modal0108 = document.getElementById("modal01-08");
-    var modal0109 = document.getElementById("modal01-09");
-    var modal0110 = document.getElementById("modal01-10");
-    var modal0111 = document.getElementById("modal01-11");
-    var modal0112 = document.getElementById("modal01-12");
-    var modal0113 = document.getElementById("modal01-13");
-    var modal0114 = document.getElementById("modal01-14");
-    var modal0115 = document.getElementById("modal01-15");
-    var modal0116 = document.getElementById("modal01-16");
-    var modal0117 = document.getElementById("modal01-17");
-    var modal0118 = document.getElementById("modal01-18");
+    
     
 
 // Get the image divs
