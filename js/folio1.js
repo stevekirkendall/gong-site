0deaac25c5a2d0b693ee8a4d0cf425c0add56ae2
Steve <skirkendall@gmail.com>
2020-06-30 16:21:29 +0100

    Modal open/closes, carousel scrolls

--- js/folio1.js
+++ js/folio1.js
@@ -1,3 +1,44 @@
+// CAROUSEL FUNCTIONALITY
+var prev = document.querySelector('.prev');
+var next = document.querySelector('.next');
+var slides = document.querySelectorAll('.carousel__slide');
+var counter = 0;
+var amount = slides.length;
+var current = slides[0];
+// const tabContent = tabby.querySelector('#' + target);
+
+var navigate = function(direction){
+    current.classList.remove('active');
+    current.classList.remove('current');
+    counter = counter + direction;
+    if(direction === -1 && counter < 0){
+        counter = amount - 1;
+    }
+    if(direction === 1 && !slides[counter]){
+        counter = 0;
+    }
+    current = slides[counter];
+    current.classList.add('active');
+    current.classList.add('current');
+};
+
+next.addEventListener('click', function(e){
+    navigate(1);
+    console.log('CLICKED NEXT!!');
+});
+
+prev.addEventListener('click', function(e){
+    navigate(-1);
+    console.log('CLICKED PREV!!');
+});
+
+navigate(0);
+
+
+
+
+
+
 /**
  * NodeList.prototype.forEach() polyfill
  * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
@@ -10,173 +51,41 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 		}
 	};
 }
-
-// Get the modals
-    var modal0101 = document.getElementById("modal01-01");
-    
-    
-
-// Get the image divs
-    var imgf101 = document.getElementById("f1-01");
-    var imgf102 = document.getElementById("f1-02");
-    var imgf103 = document.getElementById("f1-03");
-    var imgf104 = document.getElementById("f1-04");
-    var imgf105 = document.getElementById("f1-05");
-    var imgf106 = document.getElementById("f1-06");
-    var imgf107 = document.getElementById("f1-07");
-    var imgf108 = document.getElementById("f1-08");
-    var imgf109 = document.getElementById("f1-09");
-    var imgf110 = document.getElementById("f1-10");
-    var imgf111 = document.getElementById("f1-11");
-    var imgf112 = document.getElementById("f1-12");
-    var imgf113 = document.getElementById("f1-13");
-    var imgf114 = document.getElementById("f1-14");
-    var imgf115 = document.getElementById("f1-15");
-    var imgf116 = document.getElementById("f1-16");
-    var imgf117 = document.getElementById("f1-17");
-    var imgf118 = document.getElementById("f1-18");
-
+// MODAL FUNCTIONALITY
+// Get the modal
+var modal = document.querySelector('.modal');
+var thumbnails = document.getElementsByClassName('masonry-layout-panel');
+var thumbs = Array.from(thumbnails);
     
 // Check for window width greater than 765px &&
 // window height greater than 400px
 var width = window.matchMedia('(min-width: 765px)');
 var height = window.matchMedia('(min-height: 400px)');
 
-
-    imgf101.onclick = function(e){
+// thumbnail opens modal
+thumbs.forEach(thumb => {
+    thumb.addEventListener('click', (e) => {
         if(width.matches && height.matches){
-            modal0101.classList.remove('shut');
+            modal.classList.remove('shut');
         } else {
-            return;
+        return;
         }
-    }
-    imgf102.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0102.classList.remove('shut');
-            } else {
-            return;
-            }    
-    }
-    imgf103.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0103.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf104.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0104.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf105.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0105.classList.remove('shut');
-            return;
-            }      
-    }
-    imgf106.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0106.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf107.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0107.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf108.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0108.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf109.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0109.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf110.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0110.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf111.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0111.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf112.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0112.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf113.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0113.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf114.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0114.classList.remove('shut');
-            } else {
-            return;
-            }     
-    }
-    imgf115.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0115.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
-    imgf116.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0116.classList.remove('shut');
-            } else {
-            return;
-            }      }
-    imgf117.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0117.classList.remove('shut');
-            } else {
-            return;
-            }     
-    }
-    imgf118.onclick = function(e){
-        if(width.matches && height.matches){
-            modal0118.classList.remove('shut');
-            } else {
-            return;
-            }      
-    }
- 
-
-// 'Close' button functionality
-
-    var close = document.querySelectorAll('.close');
-
-    var displayNone = function(e){
-      e.target.parentElement.parentElement.classList.add('shut');
-    }
-
-    close.forEach(function(x){
-      x.addEventListener('click', displayNone);
     });
+});
+
+// 'Close' button closes modal
+var close = document.querySelectorAll('.close');
+var displayNone = function(e){
+    e.target.parentElement.parentElement.classList.add('shut');
+}
+
+close.forEach(function(x){
+    x.addEventListener('click', displayNone);
+});
+
+// modal closes modal
+modal.addEventListener('click', e => {
+if(!e.target.closest('.carousel')){
+    modal.classList.add('shut');
+}
+});
